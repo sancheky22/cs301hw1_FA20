60712fcc2036315fd76bd7f180404d1a4e4fe969
Kyle Sanchez <sancheky22@up.edu>
2020-09-21 10:07:22 -0700

    Fixed 3 ints for rgb in Face.java (HW1A graded comments). TODO: Add text view for textview!

--- app/src/main/java/com/example/cs301hw1_fa20/Face.java
+++ app/src/main/java/com/example/cs301hw1_fa20/Face.java
@@ -1,6 +1,8 @@
 // @author Kyle Sanchez
 package com.example.cs301hw1_fa20;
 
+import android.graphics.Color;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Face {
@@ -26,10 +28,23 @@ public class Face {
      */
 
     public void randomize(){
+        // creates random rgb values (0-255) for skin,eye,hair
+        int skinR = ThreadLocalRandom.current().nextInt(0,255);
+        int skinG = ThreadLocalRandom.current().nextInt(0,255);
+        int skinB = ThreadLocalRandom.current().nextInt(0,255);
+
+        int eyeR = ThreadLocalRandom.current().nextInt(0,255);
+        int eyeG = ThreadLocalRandom.current().nextInt(0,255);
+        int eyeB = ThreadLocalRandom.current().nextInt(0,255);
+
+        int hairR = ThreadLocalRandom.current().nextInt(0,255);
+        int hairG = ThreadLocalRandom.current().nextInt(0,255);
+        int hairB = ThreadLocalRandom.current().nextInt(0,255);
+
         // creates random colors from within specific range
-        int randSkin = ThreadLocalRandom.current().nextInt(0,255);
-        int randEye = ThreadLocalRandom.current().nextInt(0,255);
-        int randHairCol  = ThreadLocalRandom.current().nextInt(0,255);
+        int randSkin = Color.argb(255,skinR,skinG,skinB);
+        int randEye = Color.argb(255,eyeR,eyeG,eyeB);
+        int randHairCol  = Color.argb(255,hairR,hairG,hairB);
         int randHairSty =  ThreadLocalRandom.current().nextInt(1,3);
 
         // setting instance variables to randomized values
